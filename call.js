0dec266f31c6a9bc5e95f7a5d9f7b1191496c206
antonclanazhary <46377725+antonclanazhary@users.noreply.github.com>
2020-03-23 08:09:41 +0700

    Update call.js

--- call.js
+++ call.js
@@ -21,8 +21,7 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/url?q=https%3A%2F%2Ftravelingjobs.org%2Fmost-beautiful-places-in-indonesia-to-visit%2F&sa=D&sntz=1&usg=AFQjCNHl6jBgUDwBh3r9SeYto42dDLAOtg"}
-if(x=="2"){xxx="/url?q=https%3A%2F%2Ftravelingjobs.org%2F7-must-visit-beautiful-places-of-singapore%2F&sa=D&sntz=1&usg=AFQjCNEsxWEOhVqkwuOfij5qN_Mc96IyGA"}a_to_ve[i].href="https://www.google.com"+xxx+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/p/dedecahyono.html"}a_to_ve[i].href="https://www.erpcloud.site"+xxx+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
